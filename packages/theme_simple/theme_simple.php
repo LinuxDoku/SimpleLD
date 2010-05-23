015dc9633bae1997d303f1da5e55c74eec8a40c7
Martin <martin@linux-doku.de>
2010-05-23 23:24:31 +0200

    Added some comments to theme_simple

--- packages/theme_simple/theme_simple.php
+++ packages/theme_simple/theme_simple.php
@@ -9,10 +9,27 @@
  * @licence  GPL
  */
 class theme_simple extends package {
+    /**
+     * Name of the package
+     * @var string $name
+     */
     public static $name = 'theme_simple';
+    /**
+     * Version of the package
+     * @var int $version
+     */
     public static $version = 0.1;
+    /**
+     * If the hooks are active set 'true'
+     * else set an array with active hooks
+     * the other ones won't be used
+     * @var bool/array
+     */
     public static $hooks = true;
 
+    /**
+     * Load the theme file
+     */
     public static function theme() {
         // now include the theme file
         include('packages/theme_simple/theme/main.php');
