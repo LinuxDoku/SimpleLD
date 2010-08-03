c23ed8385dcd0fa524ce0268be0e656885a8eaeb
Martin <martin@linux-doku.de>
2010-08-03 10:09:30 -0700

    Removed old language strings from connect erros. Changed var names and comments.

--- packages/core/classes/db.php
+++ packages/core/classes/db.php
@@ -9,48 +9,56 @@
  */
 class db
 {
+  /**
+   * Connect to database
+   * 
+   * @param  string $db_host
+   * @param  string $db_user
+   * @param  string $db_pwd
+   * @param  string $db_db
+   */
   public static function connect($db_host, $db_user, $db_pwd, $db_db)
   {
         // connect to server
-        $db_connection = mysql_connect($db_host, $db_user, $db_pwd) or die ($lang['db_connection_error']." ".mysql_error());
+        $db_connection = mysql_connect($db_host, $db_user, $db_pwd) or die (mysql_error());
         // select db
-        mysql_select_db($db_db, $db_connection) or die ($lang['db_connection_error']." ".mysql_error());
+        mysql_select_db($db_db, $db_connection) or die (mysql_error());
   }
 
   /**
    * Run a normal query
    *
-   * @param  string $db_sql
+   * @param  string $sql
    * @return string
    */
-  public static function query($db_sql)
+  public static function query($sql)
   {
-        $query_ret = mysql_query($db_sql) or die (mysql_error());
-        return $query_ret;
+        $query = mysql_query($sql) or die (mysql_error());
+        return $query;
   }
 
   /**
    * Count all rows of the result
    *
-   * @param  string $db_sql
+   * @param  string $sql
    * @return int
    */
-  public static function num_rows($db_sql)
+  public static function num_rows($sql)
   {
-        $rows_ret = mysql_num_rows($db_sql);
-        return $rows_ret;
+        $rows = mysql_num_rows($sql);
+        return $rows;
   }
 
   /**
    * Fetches all rows into an array
    *
-   * @param  string $db_sql
+   * @param  string $sql
    * @return array
    */
-  public static function fetch_array($db_sql)
+  public static function fetch_array($sql)
   {
-        $array_ret = mysql_fetch_array($db_sql);
-        return $array_ret;
+        $array = mysql_fetch_array($sql);
+        return $array;
   }
 }
 ?>
